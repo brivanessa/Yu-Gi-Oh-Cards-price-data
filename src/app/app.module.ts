d5e5c20415287fc069a188c26853ea061ee13d0f
brivanessa <vanessaah.lu@gmail.com>
2022-11-30 22:34:20 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ExampleComponent } from './components/example/example.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ExampleComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
